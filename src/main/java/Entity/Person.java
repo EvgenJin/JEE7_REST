bfa756a99b522e443e505b0a1b821edacc717c0e
EvgenJin <rumbiroid@gmail.com>
2019-10-04 17:08:33 +0500

    1
    1

--- src/main/java/Entity/Person.java
+++ src/main/java/Entity/Person.java
@@ -31,6 +31,7 @@ import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 //https://support.dbagenesis.com/knowledge-base/increase-memory-target-in-oracle/#query-to-find-sga-components-size
+//https://oracle-patches.com/oracle/prof/3077-%D0%B0%D0%B2%D1%82%D0%BE%D0%BC%D0%B0%D1%82%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%BE%D0%B5-%D1%83%D0%BF%D1%80%D0%B0%D0%B2%D0%BB%D0%B5%D0%BD%D0%B8%D0%B5-%D0%BF%D0%B0%D0%BC%D1%8F%D1%82%D1%8C%D1%8E-%D0%B1%D0%B0%D0%B7%D1%8B-%D0%B4%D0%B0%D0%BD%D0%BD%D1%8B%D1%85-oracle
 /**
  *
  * @author eshahov
